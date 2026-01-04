ed25a763d830c31ac3d84f78bb7c7bbf4fc579c3
Le Hoang Vu <153077734+vule0309@users.noreply.github.com>
2026-01-04 12:28:09 +0700

    Delete foodorderav directory

--- foodorderav/data/model/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.foodorderav.data.model
-
-data class User(
-    val uid: String = "",
-    val email: String = "",
-    val displayName: String = ""
-)
--- foodorderav/util/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.foodorderav.util
-
-object Constants {
-    const val COLLECTION_FOODS = "foods"
-    const val COLLECTION_ORDERS = "orders"
-    const val COLLECTION_USERS = "users"
-}
